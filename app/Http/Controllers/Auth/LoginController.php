1771c7a303e91ef6c31aaf76c8de8e8aeb9e2e78
Dharmendra-softgrid <dharmendra@softgrid.in>
2024-10-07 10:16:23 +0530

    Login error validation added

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
-
+use Illuminate\Validation\ValidationException;
 class LoginController extends Controller
 {
     /*
@@ -44,5 +44,12 @@ class LoginController extends Controller
         $this->guard()->logout();
         $request->session()->invalidate();
         return $this->loggedOut($request) ?: redirect()->back();
+    }
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        throw ValidationException::withMessages([
+            $this->username() => [trans('auth.failed')],
+            'password' => ['The user name or password you entered is incorrect.'], // Custom password error message
+        ]);
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,124 +2,128 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Panasonic</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
+    <title>Panasonic</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
 
-  <!-- Favicons -->
-  <link rel="shortcut icon" type="image/ico" href="{{asset('/')}}images/favicon.ico"/>
+    <!-- Favicons -->
+    <link rel="shortcut icon" type="image/ico" href="{{ asset('/') }}images/favicon.ico" />
 
-  <!-- Google Fonts -->
-  <link href="https://fonts.gstatic.com" rel="preconnect">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="{{asset('/')}}assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="{{ asset('/') }}assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-  <!-- Template Main CSS File -->
-  <link href="{{asset('/')}}assets/css/style.css" rel="stylesheet">
+    <!-- Template Main CSS File -->
+    <link href="{{ asset('/') }}assets/css/style.css" rel="stylesheet">
 
-  <!-- =======================================================
+    <!-- =======================================================
   * Template Name: NiceAdmin - v2.4.1
   * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
   * Author: BootstrapMade.com
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
-  <style type="text/css">
-    .logo img {
-        max-height: 26px;
-        margin-right: 6px;
-    }
-  </style>
+    <style type="text/css">
+        .logo img {
+            max-height: 26px;
+            margin-right: 6px;
+        }
+    </style>
 </head>
 
 <body>
 
-  <main>
-    <div class="container">
-
-      <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+    <main>
         <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
 
-              <div class="d-flex justify-content-center py-4">
-                <a href="index.html" class="logo d-flex align-items-center w-auto">
-                  <!-- <img src="{{asset('/')}}images/logo.svg" alt=""> -->
-                  <span class="d-none d-lg-block">Panasonic</span>
-                </a>
-              </div><!-- End Logo -->
+            <section
+                class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
 
-              <div class="card mb-3">
+                            <div class="d-flex justify-content-center py-4">
+                                <a href="index.html" class="logo d-flex align-items-center w-auto">
+                                    <!-- <img src="{{ asset('/') }}images/logo.svg" alt=""> -->
+                                    <span class="d-none d-lg-block">Panasonic</span>
+                                </a>
+                            </div><!-- End Logo -->
 
-                <div class="card-body">
+                            <div class="card mb-3">
 
-                  <div class="pt-4 pb-2">
-                    <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
-                    <p class="text-center small">Enter your username & password to login</p>
-                  </div>
+                                <div class="card-body">
 
-                    <form method="POST" class="row g-3 needs-validation" action="{{ route('login') }}">
-                        {{ csrf_field() }}
+                                    <div class="pt-4 pb-2">
+                                        <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
+                                        <p class="text-center small">Enter your username & password to login</p>
+                                    </div>
 
-                        <div class="col-12 {{ $errors->has('email') ? ' has-error' : '' }}">
-                          <label for="yourUsername" class="form-label">E-Mail Address</label>
-                          <div class="input-group has-validation">
-                            <span class="input-group-text" id="inputGroupPrepend">@</span>
-                            <input type="email" name="email" class="form-control" placeholder="Email" autofocus="true" value="{{ old('email') }}" required autofocus>
-                            @if ($errors->has('email'))
-                                <span class="invalid-feedback">
-                                    <strong>{{ $errors->first('email') }}</strong>
-                                </span>
-                            @endif
-                          </div>
+                                    <form method="POST" class="row g-3 needs-validation" action="{{ route('login') }}">
+                                        {{ csrf_field() }}
+                                        <div class="col-12 {{ $errors->has('email') ? ' has-error' : '' }}">
+                                            <label for="yourUsername" class="form-label">E-Mail Address</label>
+                                            <div class="input-group has-validation">
+                                                <span class="input-group-text" id="inputGroupPrepend">@</span>
+                                                <input type="email" name="email"
+                                                    class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}"
+                                                    placeholder="Email" value="{{ old('email') }}" required>
+                                                @if ($errors->has('email'))
+                                                    <span class="invalid-feedback">
+                                                        <strong>{{ $errors->first('email') }}</strong>
+                                                    </span>
+                                                @endif
+                                            </div>
+                                        </div>
+
+                                        <div class="col-12 {{ $errors->has('password') ? ' has-error' : '' }}">
+                                            <label for="yourPassword" class="form-label">Password</label>
+                                            <input id="password" type="password"
+                                                class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}"
+                                                name="password" placeholder="Password" required>
+                                            @if ($errors->has('password'))
+                                                <span class="invalid-feedback">
+                                                    <strong>{{ $errors->first('password') }}</strong>
+                                                </span>
+                                            @endif
+                                        </div>
+
+                                        <div class="col-12">
+                                            <button class="btn btn-primary w-100" type="submit">Login</button>
+                                        </div>
+                                    </form>
+
+                                </div>
+                            </div>
+
+                            <div class="copyright">
+                                &copy;Copyright <strong><span>Panasonic</span></strong>. All Rights Reserved
+                            </div>
                         </div>
-
-                        <div class="col-12 {{ $errors->has('password') ? ' has-error' : '' }}">
-                          <label for="yourPassword" class="form-label">Password</label>
-                            <input id="password" type="password" class="form-control" name="password" placeholder="Password" required>
-                            @if ($errors->has('password'))
-                                <span class="invalid-feedback">
-                                    <strong>{{ $errors->first('password') }}</strong>
-                                </span>
-                            @endif
-                        </div>
-
-                        
-                        <div class="col-12">
-                          <button class="btn btn-primary w-100" type="submit">Login</button>
-                        </div>
-
-                        
-                    </form>
+                    </div>
                 </div>
-              </div>
 
-               <div class="copyright">
-                &copy;Copyright <strong><span>Panasonic</span></strong>. All Rights Reserved
-              </div>
-            </div>
-          </div>
+            </section>
+
         </div>
+    </main><!-- End #main -->
 
-      </section>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
 
-    </div>
-  </main><!-- End #main -->
+    <!-- Vendor JS Files -->
 
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+    <script src="{{ asset('/') }}assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-  <!-- Vendor JS Files -->
+    <script src="{{ asset('/') }}assets/vendor/php-email-form/validate.js"></script>
 
-  <script src="{{asset('/')}}assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
- 
-  <script src="{{asset('/')}}assets/vendor/php-email-form/validate.js"></script>
+    <!-- Template Main JS File -->
 
-  <!-- Template Main JS File -->
-  
 
 </body>
 
